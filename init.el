976891dfa9b8705e909f6fd5223cb7c1fbe9cd35
dnxbjyj <dnxbjyj@126.com>
2018-09-03 23:03:55 +0800

    modify init.el

--- init.el
+++ init.el
@@ -1,68 +1,12 @@
-
-;; 快速跳到某一行
-(global-set-key (kbd "C-c j") 'goto-line)
-
-;; dired模式默认递归删除目录
-(setq dired-recursive-deletes 'always)
-(setq dired-recursive-copies 'always)
-
-;; org-mode自动换行
-(add-hook 'org-mode-hook
-	  (lambda()
-	    (setq truncate-lines nil))) 
-
-;; TODO 设置文本文件的默认编码
- 
-;; 开启自动补全
-; 开启全局 Company 补全
-;;(global-company-mode 1)
-;;(add-hook 'after-init-hook 'global-company-mode)
-
-;; 关闭哔哔告警音
-(setq ring-bell-function 'ignore)
-
- ;; 开启括号配对
-(electric-pair-mode t)
-
-;; 有道词典，Enable Cache
-(setq url-automatic-caching t)
-(global-set-key (kbd "C-q") 'youdao-dictionary-search-at-point+)
-
-;;******************************************************************************
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;; melpa插件库配置
-(package-initialize)
-
-(require 'package)
-;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages") t)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-;;******************************************************************************
-
-;; 启用 dired-x 可以让每一次进入 Dired 模式时，使用新的快捷键 C-x C-j 就可以进 入当前文件夹的所在的路径
-(require 'dired-x)
-
-;; 让dired模式多个目录同时打开时共用一个缓冲区
-(put 'dired-find-alternate-file 'disabled nil)
-
-;; 主动加载 Dired Mode
-;; (require 'dired)
-;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
-
-;; 延迟加载dired mode
-(with-eval-after-load 'dired
-    (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+;;***************************系统类配置
+;; 定义一个函数：快速打开配置文件
+(defun open-init-file()
+    (interactive)
+    (find-file "~/.emacs.d/init.el"))
+;; 把函数open-init-file绑定到快捷键<F2>键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+;; 快速加载init.el
+(global-set-key (kbd "C-<f5>") 'load-file)
 
 ;; 自定义环境变量
 (custom-set-variables
@@ -75,6 +19,9 @@
    (quote
     (org-pomodoro w3m pydoc markdown-mode jdee company youdao-dictionary))))
 
+;; 关闭哔哔告警音
+(setq ring-bell-function 'ignore)
+
 ;; 自定义字体设置
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -83,16 +30,63 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "微软雅黑" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
+;;***************************文档编辑操作类配置
+
 ;; 显示行号
 (global-linum-mode 1)
 
-;; 定义一个函数：快速打开配置文件
-(defun open-init-file()
-    (interactive)
-    (find-file "~/.emacs.d/init.el"))
-    
-;; 把函数open-init-file绑定到快捷键<F2>键上
-(global-set-key (kbd "<f2>") 'open-init-file)
+;; 快速跳到某一行
+(global-set-key (kbd "C-c j") 'goto-line)
+;; 开启括号配对
+(electric-pair-mode t)
 
-;; 快速加载init.el
-(global-set-key (kbd "C-<f5>") 'load-file)
+;;***************************插件配置
+;;##### melpha
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; melpa插件库配置
+(package-initialize)
+(require 'package)
+;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages") t)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+;;##### 有道词典，开启缓存和设置快捷键
+(setq url-automatic-caching t)
+(global-set-key (kbd "C-q") 'youdao-dictionary-search-at-point+)
+
+;;***************************特定模式配置
+;;#####dired模式
+;; dired模式默认递归删除目录
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+
+;; 主动加载 Dired Mode
+;; (require 'dired)
+;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+
+;; 延迟加载dired mode
+(with-eval-after-load 'dired
+    (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+
+;; 启用 dired-x 可以让每一次进入 Dired 模式时，使用新的快捷键 C-x C-j 就可以进 入当前文件夹的所在的路径
+(require 'dired-x)
+
+;; 让dired模式多个目录同时打开时共用一个缓冲区
+(put 'dired-find-alternate-file 'disabled nil)
+
+;;#####org-mode
+;; org-mode自动换行
+(add-hook 'org-mode-hook
+	  (lambda()
+	    (setq truncate-lines nil)))
+
