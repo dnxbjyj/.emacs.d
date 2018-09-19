0261f42404a54547df19d5040e222e8f4f1fc9c3
dnxbjyj <dnxbjyj@126.com>
2018-09-19 22:33:55 +0800

    daily commit

--- site-lisp/config/init/init-quick-access-local.el
+++ site-lisp/config/init/init-quick-access-local.el
@@ -8,7 +8,20 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Code:
+;; 在文件夹中打开.emacs.d目录: C-S-<f1>
+(global-set-key (kbd "C-S-<f1>") '(lambda() (interactive) (shell-command "start C:/Users/Administrator.PC-20170728DWIF/AppData/Roaming/.emacs.d")))
 
+;; 快速在dired模式打开e:/code目录: C-<f6>
+(global-set-key (kbd "C-<f6>") (lambda() (interactive) (dired "e:/code")))
+;; 快速在文件夹中打开e:/code目录: C-S-<f6>
+(global-set-key (kbd "C-S-<f6>") (lambda() (interactive) (shell-command "start e:/code")))
+
+;; 快速打开vector8日程文件: <f8>
+(global-set-key (kbd "<f8>") (lambda() (interactive) (find-file "e:/gitee/vector8/vector8.org.txt")))
+;; 快速在dired模式打开e:/gitee目录: C-<f8>
+(global-set-key (kbd "C-<f8>") (lambda() (interactive) (dired "e:/gitee")))
+;; 快速在文件夹中打开e:/gitee目录: C-S-<f8>
+(global-set-key (kbd "C-S-<f8>") (lambda() (interactive) (shell-command "start e:/gitee")))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- site-lisp/config/init/init-quick-access.el
+++ site-lisp/config/init/init-quick-access.el
@@ -1,5 +1,5 @@
 ;;;;;;;;;; init-quick-access模块 ;;;;;;;;;;
-;;;; Desc: 快速访问按键配置
+;;;; Desc: 通用的快速访问按键配置
 
 ;;;; init-quick-access start here
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -9,8 +9,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Code:
 
-;; 在文件夹中打开.emacs.d目录: C-S-<f1>
-(global-set-key (kbd "C-S-<f1>") '(lambda() (interactive) (shell-command "start C:/Users/Administrator.PC-20170728DWIF/AppData/Roaming/.emacs.d")))
 ;; 在dired模式中打开.emacs.d目录: C-<f1>
 (global-set-key (kbd "C-<f1>") '(lambda() (interactive) (dired "~/.emacs.d/")))
 
@@ -19,21 +17,8 @@
 ;; 快速加载init-config.el: C-<f2>
 (global-set-key (kbd "C-<f2>") '(lambda() (interactive) (load-file "~/.emacs.d/site-lisp/config/init-config.el")))
 
-;; 快速在dired模式打开e:/code目录: C-<f6>
-(global-set-key (kbd "C-<f6>") (lambda() (interactive) (dired "e:/code")))
-;; 快速在文件夹中打开e:/code目录: C-S-<f6>
-(global-set-key (kbd "C-S-<f6>") (lambda() (interactive) (shell-command "start e:/code")))
-
-;; 快速打开vector8日程文件: <f8>
-(global-set-key (kbd "<f8>") (lambda() (interactive) (find-file "e:/gitee/vector8/vector8.org.txt")))
-;; 快速在dired模式打开e:/gitee目录: C-<f8>
-(global-set-key (kbd "C-<f8>") (lambda() (interactive) (dired "e:/gitee")))
-;; 快速在文件夹中打开e:/gitee目录: C-S-<f8>
-(global-set-key (kbd "C-S-<f8>") (lambda() (interactive) (shell-command "start e:/gitee")))
-
 ;; 快速提交git代码: <f12>
 (global-set-key (kbd "<f12>") (lambda() (interactive) (shell-command "start auto-commit.bat")))
-
 
 ;; 在文件夹中打开当前文件所在目录: C-c o
 (global-set-key (kbd "C-c o") '(lambda() (interactive) (shell-command "start .\\")))
