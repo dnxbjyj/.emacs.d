7865f1d094419f58149101fe976b1abc02560394
m2fox <dnxbjyj@126.com>
2019-01-13 23:12:58 +0800

    add 4 useful function of mine.

--- site-lisp/config/init/common/init-my-function.el
+++ site-lisp/config/init/common/init-my-function.el
@@ -8,6 +8,47 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Code:
+;; select word at current point
+(defun select-current-word ()
+  "Select word at current point."
+  (interactive)
+  (setq bounds (bounds-of-thing-at-point 'word))
+  (setq start (car bounds))
+  (setq end (cdr bounds))
+  (ignore-errors
+    (goto-char end)
+    (set-mark start)))
+(global-set-key (kbd "C-?") 'select-current-word)
+
+;; select current line
+(defun select-current-line ()
+  "Select current line."
+  (interactive)
+  (end-of-line)
+  (set-mark (line-beginning-position)))
+(global-set-key (kbd "C-_") 'select-current-line)
+
+;; copy current line to clipboard
+(defun copy-current-line ()
+  "Copy current line to clipboard."
+  (interactive)
+  (select-current-line)
+  (kill-ring-save (line-beginning-position) (line-end-position))
+  (message "current line has been copied to clipboard!"))
+(global-set-key (kbd "C-c C-l") 'copy-current-line)
+
+;; run current buffer elisp code, output to another buffer named "output"
+(defun eval-current-buf-elisp ()
+  "Run current buffer elisp code, output to another buffer named 'output'."
+  (interactive)
+  (setq cur-buf (current-buffer))
+  (get-buffer-create "output")
+  (switch-to-buffer-other-window "output")
+  (erase-buffer)
+  (eval-buffer cur-buf (get-buffer-create "output"))
+  (other-buffer -1))
+(global-set-key (kbd "C-<f9>") 'eval-current-buf-elisp)
+
 ;; 正则替换: C-c f
 (global-set-key (kbd "C-c f")
 		(lambda (from to)
