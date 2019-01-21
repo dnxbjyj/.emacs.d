3cbe1fd39fc153e6c15b662a5a687c74266721c5
m2fox <dnxbjyj@126.com>
2019-01-21 13:42:43 +0800

    Update workbench.el

--- emacs-learn/elisp/workbench.el
+++ emacs-learn/elisp/workbench.el
@@ -1,24 +1,4 @@
-;; 判断一个整数是不是素数
-(defun is-prime (n)
-  (interactive)
-  (let ((i 2) (prime-flag t))
-    (catch 'break
-      (while (<= i (+ 1 (sqrt n)))
-	(if (= (% n i) 0)
-	    (progn
-	      (setq prime-flag nil)	      
-	      (throw 'break nil)))
-	(incf i))
-      )
-    (print prime-flag)))
-
-;; 求max以内的所有素数
-(defun print-primes (max)
-  (interactive)
-  (let ((i 2))
-    (while (<= i max)
-      (if (is-prime i)
-	  (print i))
-      (incf i))))
-
-(print-primes 100)
+(with-current-buffer
+    (url-retrieve-synchronously "http://fanyi.youdao.com/openapi.do?keyfrom=YouDaoCV&key=659600698&type=data&doctype=json&version=1.1&q=cat")
+  (set-buffer-multibyte t)
+  (buffer-string))
