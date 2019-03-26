eb35ff76762d26f86d5bf607e62d4d7ca589459e
dnxbjyj <dnxbjyj@126.com>
2019-03-26 13:50:07 +0800

    add quick access to quick-access.org

--- site-lisp/config/init/common/init-quick-access-org.el
+++ site-lisp/config/init/common/init-quick-access-org.el
@@ -8,17 +8,17 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Code:
-(defun bind-key-for-open-dir (path key-str in-where)
-  "Quickly make a key bind for the operation of opening a directory in emacs dired-mode or in system explorer."
-  ;;(interactive)
+(defun bind-key-for-open (path key-str in-where)
+  "Quickly make a key bind for the operation of opening a directory or file in emacs dired-mode, or in system explorer."
+  (interactive)
   (cond
+   ((string-equal in-where "file") (global-set-key (kbd key-str) '(lambda() (interactive) (find-file path))))
    ((string-equal in-where "dired") (global-set-key (kbd key-str) '(lambda() (interactive) (dired path))))
    ((string-equal in-where "explorer") (global-set-key (kbd key-str) '(lambda() (interactive) (async-shell-command (format "start \"\" \"%s\"" path)))))
    (t (message "NOT support the operation: %s!" in-where))))
 
 ;; quickly open quick-access.org
-;;(bind-key-for-open-dir "~/.emacs.d/site-lisp/org/quick-access.org" "C-<f1>" "dired")
-(bind-key-for-open-dir "d:/" "C-<f4>" "dired")
+(bind-key-for-open-dir "~/.emacs.d/site-lisp/org/quick-access.org" "C-<f1>" "file")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Provide:
