b3090fe6f10c67504a818dc4d15ba9996f085e72
m2fox <m2fox@m2foxdeMacBook-Pro.local>
2020-01-05 17:44:19 +0800

    test input msg

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,13 +1,18 @@
 # pull latest code
+echo "start to pull"
 git pull origin master
 # input commit message
+read -p "Input commit message: " msg
 # show git status
 git status
 # add all changing
+echo "start to add"
 git add -A
 # local commit
-git commit -m "update .emacs.d"
+echo "start to commit"
+git commit -m "$msg"
 # push to remote repository
+echo "start to push to origin"
 git push origin master
 # echo msg
 echo "commit and push success!"
