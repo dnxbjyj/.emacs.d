f896823194b9120cbbc8180260e78d147f04656b
m2fox <m2fox@m2foxdeMacBook-Pro.local>
2020-01-05 17:45:45 +0800

    update auto-commit.sh

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,18 +1,18 @@
 # pull latest code
-echo "start to pull"
+echo ">>>>> start to pull"
 git pull origin master
 # input commit message
-read -p "Input commit message: " msg
+read -p ">>>>> Input commit message: " msg
 # show git status
 git status
 # add all changing
-echo "start to add"
+echo ">>>>> start to add"
 git add -A
 # local commit
-echo "start to commit"
+echo ">>>>> start to commit"
 git commit -m "$msg"
 # push to remote repository
-echo "start to push to origin"
+echo ">>>>> start to push to origin"
 git push origin master
 # echo msg
-echo "commit and push success!"
+echo ">>>>> commit and push success!"
